88f14a94dc48505c7ac27cb2ea8d17d26625e6b9
giri-rohan <rohangiri005@gmail.com>
2025-07-27 19:52:12 +0530

    committed

--- sql3/13th.sql
+++ sql3/13th.sql
@@ -1,11 +1,28 @@
--- 13.DISPLAY LOCATION OF EMPLOYEE WHOSE SALARY IS MINIMUM SALARY BUT SALARY IS GREATER THAN 2000 
-
+- - 13.DISPLAY LOCATION OF EMPLOYEE WHOSE SALARY IS MINIMUM SALARY BUT SALARY IS GREATER THAN 2000
 select
- loc from 
- scott_db.dept
-  where deptno in
-    (select deptno from scott_db.emp 
-    where sal in 
-        (select min(sal) from scott_db.emp
-         where sal in
-            (select sal from scott_db.emp where sal>2000)));
+    loc
+from
+    scott_db.dept
+where
+    deptno in (
+        select
+            deptno
+        from
+            scott_db.emp
+        where
+            sal in (
+                select
+                    min(sal)
+                from
+                    scott_db.emp
+                where
+                    sal in (
+                        select
+                            sal
+                        from
+                            scott_db.emp
+                        where
+                            sal > 2000
+                    )
+            )
+    );
