602e38843eb9ae8d2d21560052839df19be58faa
giri-rohan <rohangiri005@gmail.com>
2025-08-19 21:54:31 +0530

    30th sql1 updated

--- sql1/30th.sql
+++ sql1/30th.sql
@@ -1,3 +1,12 @@
--- 30)WAQTD NAME OF THE EMPLOYEES WHOS NAME STARTS WITH A ‘VOWEL’
+-- 30) WAQTD NAME OF THE EMPLOYEES WHOS NAME STARTS WITH A ‘ VOWEL ’
 
-select ename from scott_db.emp where ename like 'A%'or ename like 'E%' or ename like 'I%' or ename like 'O%' or ename like 'U%';
+select
+    ename
+from
+    scott_db.emp
+where
+    ename like 'A%'
+    or ename like 'E%'
+    or ename like 'I%'
+    or ename like 'O%'
+    or ename like 'U%';
